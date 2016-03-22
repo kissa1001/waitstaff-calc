47295d07ed0c05512711a04b0755ca18c07a0bee
kissa1001 <kissa101295@gmail.com>
2016-03-22 11:44:06 -0400

    charges fixed

--- app.js
+++ app.js
@@ -5,21 +5,13 @@ var app = angular.module('waitstaff-calc',[])
 			$scope.mealDetail.base = $scope.mealPrice;
 			$scope.mealDetail.tax = ($scope.taxRate/100);
 			$scope.mealDetail.tipPer = ($scope.tipPer/100);
+			$scope.total=($scope.mealDetail.base * $scope.mealDetail.tax) + $scope.mealDetail.base;
+			$scope.tip  = $scope.total * $scope.mealDetail.tipPer;
+			$scope.subTotal = $scope.tip + $scope.total;
+			$scope.tipTotal += $scope.tip;
 			$scope.taxTotal();
 			$scope.mealPrice = ''; 
 			$scope.taxRate = '';
 			$scope.tipPer = '';
 		};
-	
-
-	$scope.taxTotal = function(){
-		var total=($scope.mealDetail.base * $scope.mealDetail.tax) + $scope.mealDetail.base;
-		$scope.totalAfterTax(total);
-	};
-
-	$scope.totalAfterTax=function(total){
-		$scope.tip  = total * $scope.mealDetail.tipPer;
-		$scope.total = $scope.tip + total;
-		$scope.tipTotal += $scope.tip;
-	};
 });
