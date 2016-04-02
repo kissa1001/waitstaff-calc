142f47cb091f7e0a2eecf5064fc11caf62440d9b
kissa1001 <kissa101295@gmail.com>
2016-04-01 21:42:14 -0400

    improved code

--- app/app.js
+++ app/app.js
@@ -1,55 +1,54 @@
 var app = angular.module('waitstaff-calc',['ngRoute','ngAnimate'])
-	.config(function($routeProvider){
-		$routeProvider
-		.when('/', {
-			templateUrl: './home.html',
-			controller: 'HomeCtrl'
-		})
-		.when('/new-meal', {
-			templateUrl:'./new-meal.html',
-			controller: 'MealCtrl'
-		})
-		.when('/my-earnings', {
-			templateUrl:'./my-earnings.html',
-			controller: 'EarningsCtrl'
-		})
-		.when('/error', {
-		      template : '<p>Error Page Not Found</p>'
-		});
-	})
-	.controller('HomeCtrl', function($scope) {
-        //empty for now
-    })
-    .controller('MealCtrl', function($rootScope, $scope) {
-    	$rootScope.waiter = new Waiter();
-    	$rootScope.meal = new Meal();
-		$rootScope.onSubmit = function(e) {
-        	$scope.waiter.addTip($scope.meal.tip());
-        	$scope.meal = new Meal();
-    	};
-        $scope.cancelSubmit=function(){
-     		$scope.meal = new Meal();
-		};
-    })
-    .controller('EarningsCtrl', function($scope, $rootScope) {
-    	if(!$rootScope.waiter){
-    		$rootScope.waiter = new Waiter();
-    	}
-        $scope.onReset = function(e) {
-        	$scope.meal = new Meal();
-        	$scope.waiter = new Waiter();
-    	};
-    })
-    .run(function($rootScope, $location, $timeout) {
-		$rootScope.$on('$routeChangeError', function() {
-			$location.path("/error");
-		});
-		$rootScope.$on('$routeChangeStart', function() {
-			$rootScope.isLoading = true;
-		});
-		$rootScope.$on('$routeChangeSuccess', function() {
-		  $timeout(function() {
-  			$rootScope.isLoading = false;
-		  }, 1000);
-		});
-	});
+.config(function($routeProvider){
+  $routeProvider
+  .when('/', {
+    templateUrl: './home.html',
+    controller: 'HomeCtrl'
+  })
+  .when('/new-meal', {
+    templateUrl:'./new-meal.html',
+    controller: 'MealCtrl'
+  })
+  .when('/my-earnings', {
+    templateUrl:'./my-earnings.html',
+    controller: 'EarningsCtrl'
+  })
+  .when('/error', {
+    template : '<p>Error Page Not Found</p>'
+  });
+})
+.factory('waiter', function(){
+  return new Waiter();
+})
+.controller('HomeCtrl', function($scope) {
+  //empty for now
+})
+.controller('MealCtrl', ['$scope', 'waiter', function($scope, waiter) {
+  $scope.meal = new Meal();
+  $scope.onSubmit = function(e) {
+    waiter.addTip($scope.meal.tip());
+    $scope.meal = new Meal();
+  };
+  $scope.cancelSubmit=function(){
+    $scope.meal = new Meal();
+  };
+}])
+.controller('EarningsCtrl', ['$scope', 'waiter', function($scope, waiter) {
+  $scope.waiter = waiter;
+  $scope.onReset = function(e) {
+    waiter.reset();
+  };
+}])
+.run(function($rootScope, $location, $timeout) {
+  $rootScope.$on('$routeChangeError', function() {
+    $location.path("/error");
+  });
+  $rootScope.$on('$routeChangeStart', function() {
+    $rootScope.isLoading = true;
+  });
+  $rootScope.$on('$routeChangeSuccess', function() {
+    $timeout(function() {
+      $rootScope.isLoading = false;
+    }, 1000);
+  });
+});
